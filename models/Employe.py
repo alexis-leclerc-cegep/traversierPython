e943c036b1ca5a2bc37a76bda709c4c2505b474a
alexis-leclerc-cegep <1942518@cegeprdl.ca>
2023-03-03 10:57:43 -0500

    interface

--- models/Employe.py
+++ models/Employe.py
@@ -2,7 +2,8 @@ import datetime
 import Personne
 
 class Employe(Personne):
-    def __init__(self, nom: str, adresse: str, ville: str, province: str, codePostal: str, telephone: str, courriel: str, noEmploye: int, NAS: str, dateEmbauche: datetime, dateArret: datetime):
+    def __init__(self, nom: str, adresse: str, ville: str, province: str, codePostal: str, telephone: str,
+                 courriel: str, noEmploye: int, NAS: str, dateEmbauche: datetime, dateArret: datetime):
         super().__init__(nom, adresse, ville, province, codePostal, telephone, courriel)
         self.noEmploye = noEmploye
         self.NAS = NAS
